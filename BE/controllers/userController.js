31c4d85066f545e0914a004932639e098b4aeee0
PhucDLHSE <danglehoangphuc2003@gmail.com>
2024-10-02 15:46:45 +0800

    edit profile for user

--- BE/controllers/userController.js
+++ BE/controllers/userController.js
@@ -1,19 +1,28 @@
 // controllers/userController.js
 
 const User = require('../models/User');
+const Address = require('../models/Address');
 
-// Lấy thông tin của user đang đăng nhập
+// Show profile of the logged-in user
 exports.getUserProfile = async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).select('-password'); // Không trả về password
-        if (!user) {
-            return res.status(404).json({ msg: 'Người dùng không tồn tại' });
-        }
-        res.json(user);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Lỗi máy chủ');
+  try {
+    // Find the user by ID (without password) and populate their addresses
+    const user = await User.findById(req.user.id).select('-password');
+    const addresses = await Address.find({ user: req.user.id });
+
+    if (!user) {
+      return res.status(404).json({ msg: 'User not found' });
     }
+
+    // Return the user profile with associated addresses
+    res.json({
+      user,
+      addresses,  // Including addresses in the response
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 };
 
 // Cập nhật thông tin của user đang đăng nhập
